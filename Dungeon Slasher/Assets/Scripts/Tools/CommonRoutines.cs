8677721f04c20ef2d7f9f6e3fe74603e51584c78
Doedeledoedel <84157343+Diedeliedee@users.noreply.github.com>
2022-11-14 21:52:43 +0100

    Imported calc, and common routines tools

---
+++ Dungeon Slasher/Assets/Scripts/Tools/CommonRoutines.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class CommonRoutines
+{
+    public static IEnumerator WaitForSeconds(float time, Event onFinish)
+    {
+        yield return new WaitForSeconds(time);
+        onFinish.Invoke();
+    }
+
+    public static IEnumerator Progression(float time, ProgressionEvent onTick, Event onFinish)
+    {
+        var timer = 0f;
+
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            onTick.Invoke(timer / time);
+            yield return null;
+        }
+        onFinish.Invoke();
+    }
+
+    public static IEnumerator DoubleProgression(float time, float subTime, Event onTick, Event onFinish)
+    {
+        var timer = 0f;
+        var subTimer = 0f;
+
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            while (subTimer < subTime)
+            {
+                subTimer += Time.deltaTime;
+                yield return null;
+            }
+            onTick.Invoke();
+            subTimer = 0f;
+        }
+        onFinish.Invoke();
+    }
+
+    public static IEnumerator CustomProgression(float time, AnimationCurve curve, ProgressionEvent onTick, Event onFinish)
+    {
+        var timer = 0f;
+
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            onTick.Invoke(curve.Evaluate(timer / time));
+            yield return null;
+        }
+        onFinish.Invoke();
+    }
+
+    public delegate void Event();
+    public delegate void ProgressionEvent(float progression);
+}
