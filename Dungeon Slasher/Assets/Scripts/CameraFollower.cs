9413b0a2db1a0c9b12754e3e711fe1e78bf40d57
Doedeledoedel <84157343+Diedeliedee@users.noreply.github.com>
2022-11-23 01:21:00 +0100

    Set camera to instant follow

--- Dungeon Slasher/Assets/Scripts/CameraFollower.cs
+++ Dungeon Slasher/Assets/Scripts/CameraFollower.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     [SerializeField] private Transform m_target;
-    [SerializeField] private float m_followTime = 0.1f;
+    [SerializeField] private float m_followTime = 0.025f;
 
     private Vector3 m_offset = Vector3.zero;
     private Vector3 m_velocity = Vector3.zero;
@@ -17,6 +17,11 @@ public class CameraFollower : MonoBehaviour
 
     private void Update()
     {
+        if (m_followTime <= 0)
+        {
+            transform.position = m_target.position + m_offset;
+            return;
+        }
         transform.position = Vector3.SmoothDamp(transform.position, m_target.position + m_offset, ref m_velocity, m_followTime);
     }
 }
