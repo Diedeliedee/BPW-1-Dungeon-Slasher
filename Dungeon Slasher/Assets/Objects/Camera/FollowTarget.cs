53dcebef92561f1a7bff7bef187d1d77a8f52afe
Joeri van Alphen <jralphen@kpnmail.nl>
2024-02-18 22:11:06 +0100

    Expanded camera follow functionality

--- Dungeon Slasher/Assets/Objects/Camera/FollowTarget.cs
+++ Dungeon Slasher/Assets/Objects/Camera/FollowTarget.cs
@@ -1,25 +1,27 @@
+using Joeri.Tools.Utilities;
 using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
 {
     [SerializeField] private Transform m_target;
+    [Space]
     [SerializeField] private float m_time = 0f;
+    [Space]
+    [SerializeField] private Vector2 m_offset = default;
+    [SerializeField] private bool m_applyOffset = false;
 
-    private Vector3 m_offset = Vector3.zero;
-    private Vector3 m_velocity = Vector3.zero;
+    private Vector3 m_velocity = default;
 
-    private void Start()
-    {
-        m_offset = transform.position - m_target.position;
-    }
+    public float time       { get => m_time; set => m_time = value; }
+    public Vector3 velocity { get => m_velocity; set => m_velocity = value; }
+    public Vector2 offset   { get => m_offset; set => m_offset = value; }
+    public bool applyOffset { get => m_applyOffset; set => m_applyOffset = value; }
 
     private void Update()
     {
-        if (m_time <= 0)
-        {
-            transform.position = m_target.position + m_offset;
-            return;
-        }
-        transform.position = Vector3.SmoothDamp(transform.position, m_target.position + m_offset, ref m_velocity, m_time);
+        var target = m_target.position + (m_applyOffset ? offset.Cubular() : Vector3.zero);
+
+        if (m_time <= 0)    transform.position = target;
+        else                transform.position = Vector3.SmoothDamp(transform.position, target, ref m_velocity, m_time);
     }
 }
