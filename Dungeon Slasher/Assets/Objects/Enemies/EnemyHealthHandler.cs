5cc33e6bdcccc3025fada6e0497818a87b7fae3d
Joeri van Alphen <jralphen@kpnmail.nl>
2024-03-04 21:29:37 +0100

    Integrated elemental damage in enemy health

--- Dungeon Slasher/Assets/Objects/Enemies/EnemyHealthHandler.cs
+++ Dungeon Slasher/Assets/Objects/Enemies/EnemyHealthHandler.cs
@@ -1,19 +1,32 @@
-using Joeri.Tools.Gameify;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class EnemyHealthHandler : HealthHandler
 {
     [SerializeField] private UnityEvent m_onStunned;
+    [SerializeField] private UnityEvent m_onFlabbergasted;
+    [Space]
+    [SerializeField] private Element m_element = Element.None;
 
     private AttackInstance m_lastHitAttackInstance = null;
 
-    public override void Damage(AttackInstance _instance)
+    public override HitCallback Damage(AttackInstance _instance)
     {
-        m_lastHitAttackInstance = _instance;
-        _instance.onAttackEnd.AddListener(OnStunComplete);
+        if (_instance.element == m_element)
+        {
+            m_lastHitAttackInstance = _instance;
+            m_lastHitAttackInstance.onAttackEnd.AddListener(OnStunComplete);
 
-        m_onStunned.Invoke();
+            m_onStunned.Invoke();
+            return HitCallback.Hit;
+        }
+        else
+        {
+            m_health.ChangeHealth(-_instance.damage / 2);
+
+            m_onFlabbergasted.Invoke();
+            return HitCallback.Fumbled;
+        }
     }
 
     private void OnStunComplete()
