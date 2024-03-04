cb504ab946b0eb4e81fa3c6192b508211cfa415c
Joeri van Alphen <jralphen@kpnmail.nl>
2024-03-04 21:33:29 +0100

    Updated effects to coincide with object pooling

---
+++ Dungeon Slasher/Assets/Objects/Particles/Effect.cs
@@ -0,0 +1,22 @@
+﻿using Joeri.Tools.Patterns.ObjectPool;
+using UnityEngine;
+
+public class Effect : PoolItem
+{
+    private Animator m_animator;
+
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    protected override void OnSpawn()
+    {
+        m_animator.Play(0, -1, 0f);
+    }
+
+    protected override void OnDespawn()
+    {
+
+    }
+}
---
+++ Dungeon Slasher/Assets/Objects/Particles/EffectTypes.cs
@@ -0,0 +1,7 @@
+public static class EffectTypes
+{
+    public static string slashLight     = "Slash Light";
+    public static string slashDark      = "Slash Dark";
+    public static string fumbleDark     = "Fumble Dark";
+    public static string fumbleLight    = "Fumble Dark";
+}
